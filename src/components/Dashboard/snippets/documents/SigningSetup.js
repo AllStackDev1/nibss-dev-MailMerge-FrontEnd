06316b2430736d2c023e4a25e2c50373c73361c9
Oluwatobi Amusan <tobi@mvxchange.com>
2020-10-07 11:21:20 +0100

    Add placeholders for PDF

--- src/components/Dashboard/snippets/documents/SigningSetup.js
+++ src/components/Dashboard/snippets/documents/SigningSetup.js
@@ -5,67 +5,120 @@ import { getImageSize } from "helpers";
 import { isFileImage } from "helpers/isFileImage";
 import { Document, Page, pdfjs } from "react-pdf";
 import styled from "styled-components";
+import { getPage } from "helpers/getPage";
 pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.js`;
 
 const SigningSetup = ({ signatories, placeholders, setPlaceholders, documentFiles }) => {
+    const [hovering, setHovering] = useState(false);
     const [signatoryDragged, setSignatoryDragged] = useState(false);
     const [numPages, setNumPages] = useState(null);
 
     const documentContainer = useRef(null);
+    const pdfContainer = useRef(null);
+    const refs = useRef([React.createRef(), React.createRef()]);
+    const refsFull = useRef([React.createRef(), React.createRef()]);
 
     const mouseUp = async (x, y) => {
-        setPlaceholders(placeholders => {
-            let userSigned = placeholders.findIndex(user => user.name === signatoryDragged.name && user.email === signatoryDragged.email);
-            if (userSigned !== -1) {
-                placeholders[userSigned] = {
-                    ...placeholders[userSigned],
-                    absolute_x_coordinate: x - documentContainer.current.getBoundingClientRect().left,
-                    absolute_y_coordinate: y - documentContainer.current.getBoundingClientRect().top,
-                }
 
-                return placeholders;
-            } else {
-                return [
-                    ...placeholders,
-                    {
+        if (documentContainer.current) {
+            setPlaceholders(placeholders => {
+                let userSigned = placeholders.findIndex(user => user.name === signatoryDragged.name && user.email === signatoryDragged.email);
+                if (userSigned !== -1) {
+                    placeholders[userSigned] = {
+                        ...placeholders[userSigned],
                         absolute_x_coordinate: x - documentContainer.current.getBoundingClientRect().left,
                         absolute_y_coordinate: y - documentContainer.current.getBoundingClientRect().top,
-                        name: signatoryDragged.name,
-                        email: signatoryDragged.email
                     }
-                ];
-            }
-        });
 
-        let imageSize = await getImageSize(documentContainer.current.querySelector('img'));
-
-        setPlaceholders(placeholders => {
-            let userSigned = placeholders.findIndex(user => user.name === signatoryDragged.name && user.email === signatoryDragged.email);
-
-            if (userSigned !== -1) {
-                placeholders[userSigned] = {
-                    ...placeholders[userSigned],
-                    x_coordinate: ((x - documentContainer.current.getBoundingClientRect().left) / documentContainer.current.offsetWidth) * imageSize.width,
-                    y_coordinate: ((y - documentContainer.current.getBoundingClientRect().top) / documentContainer.current.offsetHeight) * imageSize.height
+                    return placeholders;
+                } else {
+                    return [
+                        ...placeholders,
+                        {
+                            absolute_x_coordinate: x - documentContainer.current.getBoundingClientRect().left,
+                            absolute_y_coordinate: y - documentContainer.current.getBoundingClientRect().top,
+                            name: signatoryDragged.name,
+                            email: signatoryDragged.email
+                        }
+                    ];
                 }
+            });
 
-                return placeholders;
-            } else {
-                return [
-                    ...placeholders,
-                    {
+            let imageSize = await getImageSize(documentContainer.current.querySelector('img'));
+
+            setPlaceholders(placeholders => {
+                let userSigned = placeholders.findIndex(user => user.name === signatoryDragged.name && user.email === signatoryDragged.email);
+
+                if (userSigned !== -1) {
+                    placeholders[userSigned] = {
+                        ...placeholders[userSigned],
                         x_coordinate: ((x - documentContainer.current.getBoundingClientRect().left) / documentContainer.current.offsetWidth) * imageSize.width,
-                        y_coordinate: ((y - documentContainer.current.getBoundingClientRect().top) / documentContainer.current.offsetHeight) * imageSize.height,
-                        name: signatoryDragged.name,
-                        email: signatoryDragged.email
+                        y_coordinate: ((y - documentContainer.current.getBoundingClientRect().top) / documentContainer.current.offsetHeight) * imageSize.height
                     }
-                ];
-            }
-        });
+
+                    return placeholders;
+                } else {
+                    return [
+                        ...placeholders,
+                        {
+                            x_coordinate: ((x - documentContainer.current.getBoundingClientRect().left) / documentContainer.current.offsetWidth) * imageSize.width,
+                            y_coordinate: ((y - documentContainer.current.getBoundingClientRect().top) / documentContainer.current.offsetHeight) * imageSize.height,
+                            name: signatoryDragged.name,
+                            email: signatoryDragged.email
+                        }
+                    ];
+                }
+            });
+        } else {
+            let info = getPage(refs.current, y - pdfContainer.current.getBoundingClientRect().top);
+            console.log(refsFull.current[info.page].current.offsetWidth)
+            console.log(refsFull.current[info.page].current.offsetHeight)
+
+            setPlaceholders(placeholders => {
+                let userSigned = placeholders.findIndex(user => user.name === signatoryDragged.name && user.email === signatoryDragged.email);
+
+                if (userSigned !== -1) {
+                    placeholders[userSigned] = {
+                        ...placeholders[userSigned],
+                        page: info.page + 1,
+                        absolute_x_coordinate: x - pdfContainer.current.getBoundingClientRect().left,
+                        absolute_y_coordinate: info.offset,
+                        x_coordinate: ((x - pdfContainer.current.getBoundingClientRect().left) / refs.current[info.page].current.offsetWidth) * refsFull.current[info.page].current.offsetWidth,
+                        y_coordinate: ((info.offset) / refs.current[info.page].current.offsetHeight) * refsFull.current[info.page].current.offsetHeight
+                    }
+
+                    return placeholders;
+                } else {
+                    return [
+                        ...placeholders,
+                        {
+                            page: info.page + 1,
+                            absolute_x_coordinate: x - pdfContainer.current.getBoundingClientRect().left,
+                            absolute_y_coordinate: info.offset,
+                            x_coordinate: ((x - pdfContainer.current.getBoundingClientRect().left) / refs.current[info.page].current.offsetWidth) * refsFull.current[info.page].current.offsetWidth,
+                            y_coordinate: ((info.offset) / refs.current[info.page].current.offsetHeight) * refsFull.current[info.page].current.offsetHeight,
+                            name: signatoryDragged.name,
+                            email: signatoryDragged.email
+                        }
+                    ];
+                }
+            });
+        }
     }
 
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
+        refs.current = refs.current.splice(0, numPages);
+        for (let i = 0; i < numPages; i++) {
+            refs.current[i] = refs.current[i] || React.createRef();
+        }
+        refs.current = refs.current.map((item) => item || React.createRef());
+
+        refsFull.current = refsFull.current.splice(0, numPages);
+        for (let i = 0; i < numPages; i++) {
+            refsFull.current[i] = refsFull.current[i] || React.createRef();
+        }
+        refsFull.current = refsFull.current.map((item) => item || React.createRef());
     }
 
     return (
@@ -75,32 +128,58 @@ const SigningSetup = ({ signatories, placeholders, setPlaceholders, documentFile
             <div className="width-100 height-1 border-bottom-gray top-margin-20"></div>
 
             <div className="full-height display-flex width-85-percent top-margin-40 border-box bottom-padding-30">
-                {documentFiles ?
-                    documentFiles.map((documentFile, index) =>
-                        <React.Fragment key={index}>
-                            {isFileImage(documentFile) ?
-                                <PageContainer key={index} ref={documentContainer} className={`${numPages === undefined || numPages === null ? 'width-75-percent' : ''} right-margin-50`}>
-                                    <img src={documentFile} className="full-width right-margin-10" alt="NIBSS Upload Document" />
-                                </PageContainer>
-                                :
-                                <Document
-                                    file={documentFile}
-                                    onLoadSuccess={onDocumentLoadSuccess}
-                                >
-                                    {Array.from(new Array(numPages), (el, index) => (
-                                        <PageContainer key={index} ref={documentContainer} className={`${numPages === undefined || numPages === null ? 'width-75-percent' : ''}right-margin-50 bottom-margin-20`}>
-                                            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-                                        </PageContainer>
-                                    ))}
-                                </Document>
-                            }
-                            {placeholders.map((placeholder, index) =>
-                                <div key={index} className="width-180 height-40 absolute" style={{ left: placeholder.absolute_x_coordinate, top: placeholder.absolute_y_coordinate, backgroundColor: getColor(placeholder.name) }}></div>
-                            )}
-                        </React.Fragment>
-                    )
-                    : ""}
-                <div className="width-25-percent">
+                <div ref={pdfContainer} className={`${numPages === undefined || numPages === null ? "min-width-70-percent" : ""} right-margin-50`}>
+                    {documentFiles ?
+                        documentFiles.map((documentFile, index) =>
+                            <React.Fragment key={index}>
+                                {isFileImage(documentFile) ?
+                                    <PageContainer key={index} ref={documentContainer} className={`${numPages === undefined || numPages === null ? 'width-75-percent' : ''}`} onMouseOver={e => { e.preventDefault(); setHovering(true); }} onMouseLeave={e => setHovering(false)}>
+                                        <img src={documentFile} className="full-width right-margin-10" alt="NIBSS Upload Document" />
+                                        {placeholders.map((placeholder, index) =>
+                                            <div key={index} className="width-180 height-40 absolute" style={{ left: placeholder.absolute_x_coordinate, top: placeholder.absolute_y_coordinate, backgroundColor: getColor(placeholder.name) }}></div>
+                                        )}
+                                    </PageContainer>
+                                    :
+                                    <>
+                                        <Document
+                                            file={documentFile}
+                                            onLoadSuccess={onDocumentLoadSuccess}
+                                        >
+                                            {Array.from(new Array(numPages), (el, index) => (
+                                                <PageContainer ref={refs.current[index]} key={index} className={`${index} ${numPages === undefined || numPages === null ? 'width-75-percent' : ''} bottom-margin-50 ${hovering ? 'one' : ''}`} onMouseOver={e => { e.preventDefault(); setHovering(true); }} onMouseLeave={e => setHovering(false)}>
+                                                    <Page width={550} key={`page_${index + 1}`} pageNumber={index + 1} />
+                                                    {placeholders.map((placeholder, i) =>
+                                                        placeholder.page === (index + 1) ?
+                                                            <div key={i} className="width-180 height-40 absolute" style={{ left: placeholder.absolute_x_coordinate, top: placeholder.absolute_y_coordinate, backgroundColor: getColor(placeholder.name) }}></div>
+                                                            : ""
+                                                    )}
+                                                </PageContainer>
+                                            ))}
+                                        </Document>
+                                        <div className="hide height-0 overflow-hidden">
+                                            <Document
+                                                file={documentFile}
+                                                onLoadSuccess={onDocumentLoadSuccess}
+                                            >
+                                                {Array.from(new Array(numPages), (el, index) => (
+                                                    <PageContainer ref={refsFull.current[index]} key={index} className={`${numPages === undefined || numPages === null ? 'width-75-percent' : ''} bottom-margin-50`} onMouseOver={e => { e.preventDefault(); setHovering(true); }} onMouseLeave={e => setHovering(false)}>
+                                                        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                                                        {placeholders.map((placeholder, i) =>
+                                                            placeholder.page === (index + 1) ?
+                                                                <div key={i} className="width-180 height-40 absolute" style={{ left: placeholder.absolute_x_coordinate, top: placeholder.absolute_y_coordinate, backgroundColor: getColor(placeholder.name) }}></div>
+                                                                : ""
+                                                        )}
+                                                    </PageContainer>
+                                                ))}
+                                            </Document>
+                                        </div>
+                                    </>
+                                }
+                            </React.Fragment>
+                        )
+                        : ""}
+                </div>
+                <SignatoriesPanel>
                     <div className="display-flex align-items-center">
                         <div className="width-40 height-40 display-flex align-items-center">
                             <img src={require(`images/icons/document/signature.svg`)} className="height-18 right-margin-20" alt="NIBSS Signature" />
@@ -130,7 +209,7 @@ const SigningSetup = ({ signatories, placeholders, setPlaceholders, documentFile
                             </div>
                         )
                         : <i className="top-margin-30 size-pointeight-rem light">No signatories selected</i>}
-                </div>
+                </SignatoriesPanel>
             </div>
         </>
     )
@@ -141,6 +220,15 @@ const PageContainer = styled.div`
     &:hover {
         border: 1px dashed #d8d8d8 !important;
     }
+`;
+
+const SignatoriesPanel = styled.div`
+    position: fixed;
+    top: 300px;
+    right: 9%;
+    height: calc(100vh - 300px);
+    overflow-y: auto;
+    width: calc(((25 * (80%)) / 100) - 50px);
 `;
 
 export default SigningSetup;
---
+++ src/helpers/getPage.js
@@ -0,0 +1,9 @@
+export function getPage(array, offset) {
+    for (let i = 0; i < array.length; i++) {
+        if(offset <= array[i].current.offsetHeight) {
+            return {page: i, offset};
+        } else {
+            offset = offset - (array[i].current.offsetHeight + 50);
+        }
+    }
+}
