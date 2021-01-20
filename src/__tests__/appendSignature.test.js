83063ae0759607399b8c05b366c21600fe290166
Samuel oriaku <oriaku.samuel@gmail.com>
2021-01-20 23:20:52 +0000

    testing

--- src/__tests__/appendSignature.test.js
+++ src/__tests__/appendSignature.test.js
@@ -1,83 +1,4 @@
-// import React from 'react';
-// import { shallow, mount } from 'enzyme';
-// import AppendSignature from "../components/Dashboard/AppendSignature"
-// import { useSelector, Provider } from 'react-redux';
-// import { createStore } from 'redux';
-// import reducer from '../reducers/documentReducer';
-
-
-// jest.mock("react-router-dom", () => ({
-//     ...jest.requireActual('react-router-dom'),
-//     useParams: () => ({ documentId: 123 }),
-// }))
-
-// jest.mock("react", () => ({
-//     ...jest.requireActual('react'),
-//     useEffect: () => ("user1", "doc123", "123456")
-// }))
-
-
-// const shallowSetup = (props = {}) => {
-
-//     jest.mock("react-redux", () => ({
-//         ...jest.requireActual("react-redux"),
-//         useSelector: jest.fn()
-//     }))
-
-
-//     const store = createStore(reducer, { document: "1234" });
-
-//     return mount(
-//         <Provider store={store}>
-//             <AppendSignature {...props} />
-//         </Provider>
-//     )
-
-// }
-
-// beforeEach(() => {
-//     useSelector.mockImplementation(callback => {
-//         return callback({ documents: { document: "1234" } });
-//     });
-
-// });
-
-// afterEach(() => {
-//     useSelector.mockClear();
-// });
-
-
-
-
-// it("should render component", () => {
-
-
-//     const wrapper = shallowSetup({ user: "user123", documentId: "1233", userToken: "11111" });
-
-//     // wrapper.setProps({ user: "user1", documentId: "1234", userToken: "token1" })
-
-//     expect(wrapper).toBeTruthy();
-//     // expect(wrapper).toHaveBeenCalledWith({ user: "user123", documentId: "1233", userToken: "11111" })
-// })
-
-// it("should render component without errors", () => {
-
-//     const wrapper = shallowSetup();
-//     expect(wrapper.find('[data-test="component-append-signature"]').length).toBe(1);
-
-// })
-
-// it("", () => {
-
-//     const stateMock = jest.fn();
-//     const useSelectorMock = jest.fn(stateMock);
-
-//     const wrapper = mount(<AppendSignature useSelector={useSelectorMock} />)
-
-// })
-
-
-import React, { useState } from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
 import AppendSignature from "../components/Dashboard/AppendSignature"
 import enableHooks from "jest-react-hooks-shallow";
@@ -92,15 +13,7 @@ jest.mock("react-router-dom", () => ({
     useParams: () => ({ documentId: 123 }),
 }))
 
-// jest.mock('react', () => ({
-//     ...jest.requireActual('react'),
-//     useState: jest.fn(),
-// }));
-
-
-// jest.mock("../components/Dashboard/AppendSignature", () => mockFunction)
-
-test("yolo", () => {
+test("should render without error", () => {
 
     jest.mock("react-redux", () => ({
         ...jest.requireActual("react-redux"),
@@ -108,10 +21,8 @@ test("yolo", () => {
     }))
 
     const setDocumentMock = jest.fn();
+    const setDocumentData = jest.fn();
 
-    // beforeEach(() => {
-    //     useState.mockImplementation(init => [{}, setDocumentMock])
-    // })
 
     afterEach(() => {
         useSelector.mockClear();
@@ -123,10 +34,10 @@ test("yolo", () => {
 
 
 
-    // console.log(<AppendSignature user="user123" documentId="1233" userToken="11111" />);
-
     const renderSignatureInterfaceMock = jest.fn();
     const wrapper = shallow(<AppendSignature renderSignatureInterface={renderSignatureInterfaceMock} />)
+
+
 
     let result = renderSignatureInterfaceMock("e");
     expect(result).toBeUndefined();
