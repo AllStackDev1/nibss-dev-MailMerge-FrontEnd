45465d98b7919eeeaab9f04cb9c8f6b86e2f58a9
Oluwatobi Amusan <Maranathatolulope@gmail.com>
2021-02-08 17:26:15 +0100

    Code smell fixes 3

--- src/components/onboarding/steps/saveSignature.js
+++ src/components/onboarding/steps/saveSignature.js
@@ -87,8 +87,7 @@ const SaveSignature = ({ add }) => {
                         signatureType={signatureType}
                         onClick={() => {
                             setSignatureType("write");
-                            // signatureCanvas.current?.clear();
-                            const signatureCanvasCurrent = signatureCanvas.current?.clear()
+                            signatureCanvas.current?.clear()
                         }}
                         optionType="write"
                         image={require(`images/icons/write-signature.svg`)} />
